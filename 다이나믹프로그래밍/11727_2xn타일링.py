de9c88bc7cbf503ad3d4ba659afd13be225f0ec0
chaewonkim <kimlove9182@gmail.com>
2024-03-02 00:04:58 +0900

    DP (폴더정리)

---
+++ 다이나믹프로그래밍/11727_2xn타일링.py
@@ -0,0 +1,13 @@
+n=int(input())
+
+sum=[0]*1001
+
+for i in range(1,n+1):
+    if i==1:
+        sum[1]=1
+    elif i==2:
+        sum[2]=3
+    else:
+        sum[i]=sum[i-1]+sum[i-2]*2
+
+print(sum[n]%10007)
