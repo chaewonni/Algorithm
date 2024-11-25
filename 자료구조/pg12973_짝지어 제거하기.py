579ae82cd755be3248f692a8c3506e502d32b396
chaewonkim <kimlove9182@gmail.com>
2024-11-25 23:56:13 +0900

    code upload

---
+++ 자료구조/pg12973_짝지어 제거하기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = 0
+    stack = []
+    for i in s:
+        if not stack:
+            stack.append(i)
+        else:
+            if stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+    if not stack:
+        answer = 1
+    else:
+        answer = 0
+
+    return answer
